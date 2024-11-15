b1deb9347a63153b45ee7efd2adf6c6ed25815ff
chrishayuk <chrishayuk@googlemail.com>
2024-11-16 00:47:37 +0200

    cleanup

--- dataset_generation/base_dataset_generator.py
+++
@@ -1,8 +0,0 @@
-# dataset_generation/base_dataset_generator.py
-class BaseDatasetGenerator:
-    def generate_data(self, graph, start, goal):
-        """
-        Generate dataset for training.
-        This method should be implemented by subclasses.
-        """
-        raise NotImplementedError("Subclasses must implement this method.")
